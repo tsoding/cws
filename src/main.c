36d3f05a9e4ca31908e5a2c44569c4619c3825d7
rexim <reximkut@gmail.com>
2021-03-17 13:11:43 +0700

    Extract repeating echo message parameters

--- src/main.c
+++ src/main.c
@@ -241,16 +241,18 @@ int main(void)
 
     // Receiving frames
     {
-        const char *hello = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        const char *const payload = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        const Cws_Message_Kind kind = CWS_MESSAGE_BIN;
+        const size_t chunk_size = 100;
 
-        cws_send_message(&cws, CWS_MESSAGE_BIN, (uint8_t*)hello, strlen(hello), 100);
+        cws_send_message(&cws, kind, (uint8_t*)payload, strlen(payload), chunk_size);
         Cws_Message message = {0};
         int ret = cws_read_message(&cws, &message);
         while (ret == 0) {
             log_message(stdout, message);
             cws_free_message(&cws, &message);
             sleep(1);
-            cws_send_message(&cws, CWS_MESSAGE_BIN, (uint8_t*)hello, strlen(hello), 100);
+            cws_send_message(&cws, kind, (uint8_t*)payload, strlen(payload), chunk_size);
             ret = cws_read_message(&cws, &message);
         }
     }
